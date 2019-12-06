8d31e77e73aab169e8f727bc623a665de301dde8
lejk8104 <lejk8104@gmail.com>
2019-12-06 09:29:03 +0900

    채팅창 수정 191206ing

--- MainPage/src/kr/ac/sunmoon/client/ChatRoom.java
+++ MainPage/src/kr/ac/sunmoon/client/ChatRoom.java
@@ -12,6 +12,8 @@ import com.gwtext.client.widgets.layout.AnchorLayoutData;
 import com.gwtext.client.widgets.layout.ColumnLayout;
 import com.gwtext.client.widgets.layout.ColumnLayoutData;
 import com.gwtext.client.widgets.layout.FormLayout;
+import com.gwtext.client.widgets.layout.HorizontalLayout;
+import com.gwtext.client.widgets.layout.VerticalLayout;
 
 public class ChatRoom extends Window{
 	public ChatRoom() {
@@ -19,7 +21,7 @@ public class ChatRoom extends Window{
 		this.setBorder(false);
 		this.setPaddings(0);
 		this.setClosable(true);
-		this.setWidth(615);
+		this.setWidth(700);
 		this.setHeight(400);
 		this.setPlain(true);
 		this.setCloseAction(this.HIDE);
@@ -28,81 +30,76 @@ public class ChatRoom extends Window{
 		final FormPanel chatroomform = new FormPanel();  
         chatroomform.setFrame(true);
         chatroomform.setTitle("Chatting Page");
-        chatroomform.setWidth(600);
+        chatroomform.setWidth(700);
         chatroomform.setLabelWidth(55);
         chatroomform.setButtonAlign(Position.CENTER);
         chatroomform.setMargins(0, 15, 0, 0);
         chatroomform.setLayout(new ColumnLayout());
         
-        //user
-        Panel imgPanel_c1 = new Panel();
-        imgPanel_c1.setLayout(new FormLayout());  
-        imgPanel_c1.setBorder(false);
+        Panel overallPanel = new Panel();
+        overallPanel.setBorder(false);
+//        overallPanel.setPaddings(15);
+        overallPanel.setLayout(new HorizontalLayout(15));
+        chatroomform.add(overallPanel);
+          
+        
+        //user1
+        Panel user1Panel = new Panel();
+        user1Panel.setLayout(new VerticalLayout(15));  
+        user1Panel.setBorder(false);
         
         Image imglogo1 = new Image();
 		imglogo1.setUrl("image/login.png");
-		imgPanel_c1.add(imglogo1, new AnchorLayoutData("97%"));
+		user1Panel.add(imglogo1, new AnchorLayoutData("97%"));
 		
-        chatroomform.add(imgPanel_c1, new ColumnLayoutData(0.2));  
+		overallPanel.add(user1Panel);  
+		Label lb1 = new Label("seiya.u77 ");
+		lb1.setPosition(30, 10);
+		user1Panel.add(lb1, new AnchorLayoutData("97%"));
+		
+		// 채팅룸
+        Panel chatroomPanel = new Panel();  
+        chatroomPanel.setLayout(new VerticalLayout(15));
+        chatroomform.setMargins(30, 0, 0, 0);
+        chatroomPanel.setBorder(false);
         
-        Panel text_Panel_c3 = new Panel();  
-        text_Panel_c3.setLayout(new FormLayout());
-        text_Panel_c3.setWidth(100);
-        text_Panel_c3.setBorder(false);
+        //메세지 막스
+        TextField messagebox = new TextField();
+        messagebox.setWidth("300");
         
-        TextBox txtinput = new TextBox();
-        txtinput.setHeight("100");
+        chatroomPanel.setHtml("<p>Send your message</p>");
         
-        text_Panel_c3.add(txtinput, new AnchorLayoutData("97%"));
-        chatroomform.add(text_Panel_c3, new ColumnLayoutData(0.6));  
+        chatroomPanel.add(messagebox, new AnchorLayoutData("97%"));
+        overallPanel.add(chatroomPanel);
         
-        Panel imgPanel_c2 = new Panel();
-        imgPanel_c2.setLayout(new FormLayout());  
-        imgPanel_c2.setBorder(false);
+        //user1
+        Panel user1Panel2 = new Panel();
+        user1Panel2.setLayout(new VerticalLayout(15));  
+        user1Panel2.setBorder(false);
         
         Image imglogo2 = new Image();
 		imglogo2.setUrl("image/login.png");
-		imgPanel_c2.add(imglogo2, new AnchorLayoutData("97%"));
+		user1Panel2.add(imglogo2, new AnchorLayoutData("97%"));
+		Label lb12 = new Label("jaekeunlee ");
+		user1Panel2.add(lb12, new AnchorLayoutData("97%"));
 		
-        chatroomform.add(imgPanel_c2, new ColumnLayoutData(0.2));
-        
-        Label lb1 = new Label("seiya.u77 ");
-        Label lb12 = new Label("jaekeunlee ");
-		
-        Panel first_Column = new Panel();  
-        first_Column.setLayout(new FormLayout());  
-        first_Column.setBorder(false);
-        first_Column.setMargins(0, 20, 0, 0);
-        
-        Panel second_Column = new Panel();  
-        second_Column.setLayout(new FormLayout());  
-        second_Column.setBorder(false); 
-        
-        first_Column.add(lb1, new AnchorLayoutData("200%"));
-        second_Column.add(lb12, new AnchorLayoutData("200%"));
-        
-        chatroomform.add(first_Column, new ColumnLayoutData(0.85));
-        chatroomform.add(second_Column, new ColumnLayoutData(0.1));
-        
+        chatroomform.add(user1Panel2);
         
         //interests panel
         Panel interest_user1 = new Panel();  
-        interest_user1.setLayout(new FormLayout());  
+        interest_user1.setLayout(new FormLayout()); 
         interest_user1.setBorder(false);
-//        interest_user1.setMargins(0, 0, 0, 0);
         
         Panel interest_user2 = new Panel();  
         interest_user2.setLayout(new FormLayout());  
         interest_user2.setBorder(false);
-        interest_user2.setMargins(0, 100, 0, 0);
-        
         
         //interest_user1
         final TextField interest1 = new TextField("Interest1", "data", 75);  
         interest1.setAllowBlank(false);  
         interest1.setValue("kjinteraction"); 
         interest_user1.add(interest1);
-        
+
         final TextField interest2 = new TextField("Interest2", "data", 75);  
         interest2.setAllowBlank(false);  
         interest2.setValue("chatting"); 
@@ -122,6 +119,8 @@ public class ChatRoom extends Window{
         interest5.setAllowBlank(false);  
         interest5.setValue("movie"); 
         interest_user1.add(interest5);
+        
+        user1Panel.add(interest_user1);
         
         //interest_user2
         final TextField interest_1 = new TextField("Interest1", "data", 75);  
@@ -149,16 +148,19 @@ public class ChatRoom extends Window{
         interest_5.setValue("reading"); 
         interest_user2.add(interest_5);
         
-        Panel chattingForm = new Panel();
-        chattingForm.setLayout(new FormLayout());  
-        chattingForm.setBorder(false);
-        chattingForm.setBodyStyle("background-color:#EEEEEE");
-        chattingForm.setMargins(0, 10, 0, 0);
+        user1Panel2.add(interest_user2);
         
+        //채팅 창
+//        Panel chattingForm = new Panel();
+//        chattingForm.setLayout(new FormLayout());  
+//        chattingForm.setBorder(false);
+//        chattingForm.setBodyStyle("background-color:#EEEEEE");
+//        chattingForm.setMargins(0, 10, 0, 0);
+//        
         
-        chatroomform.add(interest_user1, new ColumnLayoutData(0.33));
+//        chatroomform.add(interest_user1, new ColumnLayoutData(0.33));
 //        chatroomform.add(chattingForm, new ColumnLayoutData(0.33));
-        chatroomform.add(interest_user2, new ColumnLayoutData(0.33));
+//        chatroomform.add(interest_user2, new ColumnLayoutData(0.33));
         
         
         this.add(chatroomform);
